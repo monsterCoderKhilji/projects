432489fb111a48214a4825a1b01bfe383f2f1671
Avishek Datta <code.avishek.datta@gmail.com>
2020-06-27 18:01:50 +0200

    End of module 2 with assignments.

---
+++ angular-hamedani/courses-example/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FormsModule } from '@angular/forms';
+import { SummaryPipe } from './summary.pipe';
+import { FavoriteComponent } from './favorite/favorite.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    SummaryPipe,
+    FavoriteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-hamedani/courses-example/src/app/courses/courses.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { CoursesService } from '../courses.service';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+
+  constructor(private coursesService: CoursesService) { }
+
+  title = 'List of Courses';
+  courses: Array<string> = [];
+  imageUrl = 'http://lorempixel.com/400/200';
+  colSpan = 3;
+  valueSpan = 3;
+  isActive = true;
+  name = '';
+  course: any = {
+    name: 'Angular',
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2020, 6, 28)
+  };
+  text = "";
+
+  ngOnInit(): void {
+    this.courses = this.getCourses();
+    this.text = this.getText();
+  }
+
+  onSave(): void {
+    this.coursesService.addCourse(this.name);
+    this.name = '';
+  }
+
+  onKeyUp(): void {
+    console.log('Value is ' + this.name);
+  }
+
+  getTitle(): string {
+    return this.title;
+  }
+
+  getCourses(): Array<string> {
+    return this.coursesService.getCourses();
+  }
+
+  getText(): string {
+    return this.coursesService.getLorem();
+  }
+
+}
